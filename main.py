8d485f4f6f4f3cee02b151cb5aaf6a07f5a04ad3
Akhil Kishore <akhilkishorek13@gmail.com>
2019-09-09 13:01:53 +0530

    update'

---
+++ main.py
@@ -0,0 +1,92 @@
+from flask import Flask, jsonify,request,render_template,make_response
+from flask import *
+import urllib.request
+import json 
+import requests
+from linkedin_api import Linkedin
+
+app = Flask(__name__)
+app.secret_key = "spc"
+
+### Credentials ##############################################################################################################
+api = Linkedin('akhilkishorek13@gmail.com', 'AKHIL12345')
+##############################################################################################################################
+
+
+##############################################################################################################################
+
+
+
+
+
+def scrapSingleUser(uri):
+    try:
+        profile = api.get_profile(uri)
+    #summary name location headline experience skills
+        user_data = []
+        user_data.append(uri)
+        if 'firstName' in profile:
+            if 'lastName' in profile:
+                user_data.append(profile['firstName']+" "+profile['lastName'])
+            else:
+                user_data.append(profile['firstName'])
+        elif 'lastName' in profile:
+            user_data.append(profile['lastName'])
+        else:
+            user_data.append("none")
+            
+        if 'headline' in profile:
+            user_data.append(profile['headline'])
+        else:
+            user_data.append("none")
+
+        if 'industryName' in profile:
+            user_data.append(profile['industryName'])
+        else:
+            user_data.append("none")
+            
+        if 'locationName' in profile:
+            user_data.append(profile['locationName'])
+        else:
+            user_data.append("none")
+
+        if 'summary' in profile:
+            user_data.append(profile['summary'].replace("\n"," "))
+        else:
+            user_data.append("none")
+        #name headline industry  location summary
+        print(user_data)
+        return user_data
+        
+    except:
+        return False
+
+
+
+
+##############################################################################################################################
+
+##############################################################################################################################
+@app.route('/')
+def index():
+    return render_template('home.html') #if no : calling index page
+
+@app.route('/userScrap', methods=['POST'])
+def login():
+    response = request.get_json()
+    print("responce shown below")
+    id_list = response['data']
+    print(len(id_list))
+    profile_details = []
+    if len(id_list) > 0:
+        for x in id_list:
+            xyz = scrapSingleUser(x) 
+            if xyz != False:
+                profile_details.append(xyz)
+        return jsonify(profile_details)
+    else:
+        return jsonify("404")
+
+##############################################################################################################################
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4500,debug=True) #port  4500 
---
+++ static/main.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+
+    function modal(){
+        $('.modal').modal('show');
+        console.log('overlay starting..');
+        
+    }
+
+    function convertArrayOfObjectsToCSV(args) {  
+        var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+        data = args.data || null;
+        if (data == null || !data.length) {
+            return null;
+        }
+
+        columnDelimiter = args.columnDelimiter || ',';
+        lineDelimiter = args.lineDelimiter || '\n';
+
+        keys = Object.keys(data[0]);
+
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+
+        data.forEach(function(item) {
+            ctr = 0;
+            keys.forEach(function(key) {
+                if (ctr > 0) result += columnDelimiter;
+
+                result += item[key];
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
+
+        return result;
+    }
+
+
+    function downloadCSV(items) {  
+        var data, filename, link;
+        var csv = convertArrayOfObjectsToCSV({
+            data: items
+        });
+
+        if (csv == null) return;
+        filename = 'export.csv';
+
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = 'data:text/csv;charset=utf-8,' + csv;
+        }
+        data = encodeURI(csv);
+
+        link = document.getElementById('asd');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click();
+
+    }
+
+    function stripUsername(text){
+        //text = text.replace(" ","");
+        temp = "";
+        temp += text[text.length-1];
+        for(x=text.length-2;x>0;x--){
+            if(text[x] == "/") {
+                break;
+            }else{
+                temp += text[x];
+            }
+        }
+        temp = reverseString(temp);
+        temp = temp.replace("/","");
+        return temp;
+    }
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }
+
+ 
+    $("#LUPS_btn").click(function(){
+        var data = $('#LUPS_text').val();
+        if(data.length > 5){
+            all_id = [];
+            data = data.split("\n");
+            for(var y=0;y<data.length;y++){
+                img = stripUsername(data[y]);
+                all_id.push(img);
+            }
+            //downloadCSV({ filename: "stock-data.csv" });
+            sub = {"data": all_id};
+            modal();
+            $.ajax({
+                url: "/userScrap",
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(sub)
+              }).done(function(data) {
+                    $('.modal').modal('hide');
+                    new_data = []
+                    //name headline industry skills location summary
+                    new_data.push(["ID", "Name", "Headline", "Industry", "Location", "Summary"])
+                    for(var x = 0; x< data.length;x++){
+                        new_data.push(data[x]);
+                    }
+                    alert(new_data);
+                    downloadCSV(new_data);
+              });
+
+
+        }else{
+            alert("no data");
+        }
+
+    });
+
+
+})
